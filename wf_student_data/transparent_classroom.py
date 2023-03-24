8373761872b8123f428f799b0f614b3ba17073ef
Theodore Quinn <tcquinn@tcquinn.org>
2023-03-24 17:18:56 -0500

    Implement methods to fetch classroom data

--- wf_student_data/transparent_classroom.py
+++ wf_student_data/transparent_classroom.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import tqdm
 import json
 import os
 import logging
@@ -36,17 +37,95 @@ class TransparentClassroomClient:
             )
             self.api_token = json_output['api_token']
     
-    def fetch_school_data(
+    def fetch_classroom_data(
         self,
-        parse=True
+        school_ids=None,
+        progress_bar=False,
+        notebook=False
     ):
-        schools = self.request(
+        if school_ids is None:
+            school_ids = self.fetch_school_ids()
+        if progress_bar:
+            if notebook:
+                school_id_iterator = tqdm.notebook.tqdm(school_ids)
+            else:
+                school_id_iterator = tqdm.tqdm(school_ids)
+        else:
+            school_id_iterator = school_ids
+        classroom_dfs = list()
+        for school_id in school_id_iterator:
+            classrooms_school = self.fetch_classroom_data_school(
+                school_id=school_id
+            )
+            classroom_dfs.append(classrooms_school)
+        classrooms = (
+            pd.concat(classroom_dfs)
+            .sort_index()
+        )
+        return classrooms
+
+    def fetch_classroom_data_school(
+        self,
+        school_id
+    ):
+        classrooms_school_list = self.request(
+            'classrooms.json',
+            params={
+                'show_inactive': 'true'
+            },
+            school_id=school_id
+        )
+        if len(classrooms_school_list) == 0:
+            logging.warning('School {} has zero classrooms'.format(
+                school_id
+            ))
+            return pd.DataFrame()
+        classrooms_school = (
+            pd.DataFrame(classrooms_school_list)
+            .assign(school_id=school_id)
+            .rename(columns={'id': 'classroom_id'})
+            .reindex(columns=[
+                'school_id',
+                'classroom_id',
+                'name',
+                'lesson_set_id',
+                'level',
+                'active'
+            ])
+            .set_index([
+                'school_id',
+                'classroom_id'
+            ])
+            .sort_index()
+        )
+        return classrooms_school
+    
+    def fetch_school_ids(self):
+        schools = self.fetch_school_data()
+        school_ids = schools.index.tolist()
+        return school_ids
+    
+    def fetch_school_data(self):
+        schools_list = self.request(
             'schools.json',
             params=None,
             school_id=None
         )
-        if parse:
-            schools = parse_school_data(schools)
+        schools = (
+            pd.DataFrame(schools_list)
+            .rename(columns={'id': 'school_id'})
+            .reindex(columns=[
+                'school_id',
+                'name',
+                'address',
+                'phone',
+                'time_zone'        
+            ])
+            .set_index([
+                'school_id'
+            ])
+            .sort_index()
+        )
         return schools
 
     def request(
@@ -79,22 +158,3 @@ class TransparentClassroomClient:
                 pass
             raise Exception(error_message)
         return r.json()
-
-def parse_school_data(
-    schools
-):
-    schools = (
-        pd.DataFrame(schools)
-        .rename(columns={'id': 'school_id'})
-        .reindex(columns=[
-            'school_id',
-            'name',
-            'address',
-            'phone',
-            'time_zone'        
-        ])
-        .set_index([
-            'school_id'
-        ])
-    )
-    return schools
